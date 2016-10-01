0ecf1df7624336580c6cf35da8d90258b326717d
Kasper Munch <kaspermunch@birc.au.dk>
2016-10-01 14:33:12 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='MultiProcessing',
+      version='1.0',
+      description='',
+      author='Kasper Munch',
+      author_email='kaspermunch@birc.au.dk',
+      packages=['MultiProcessing'],
+     )
